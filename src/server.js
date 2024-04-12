7b93a6c3943512264d0ee4b8dabc728efba1d565
Maria Alice <alicedflopes.02@gmail.com>
2024-04-12 16:46:52 -0300

    feat(api): add base project
    Add the base for users CRUD routes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,20 @@ import http from 'node:http' //ES Modules
 
 //Create Native Server
 const server = http.createServer((request, response) => {
+    //TODO: Get Request Method & URL
+    const {method, url} = request //destructuring
+
+    //TODO: Create Users CRUD Routes
+
+    if(method == 'POST' && url == '/users') {
+        //early return
+        return response.end('Create Users')
+    }
+
+    if(method == 'GET' && url == '/users') {
+        return response.end('Read Users')
+    }
+
     return response.end('Hello World!') //Create HTML Page
 })
 
