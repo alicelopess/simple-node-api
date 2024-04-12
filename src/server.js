2fcef8c455e8b23d558d09bd88fbe7bc9b3fb4f9
Maria Alice <alicedflopes.02@gmail.com>
2024-04-12 17:34:08 -0300

    feat(api): add header and status code
    Add request header and response status code to user CRUD routes

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import http from 'node:http' //ES Modules
 
+//Create temporary DB
+const users = []
+
 //Create Native Server
 const server = http.createServer((request, response) => {
     //TODO: Get Request Method & URL
@@ -8,15 +11,26 @@ const server = http.createServer((request, response) => {
     //TODO: Create Users CRUD Routes
 
     if(method == 'POST' && url == '/users') {
-        //early return
-        return response.end('Create Users')
+        users.push({
+            id: 1,
+            name: 'Alice Dantas',
+            email: 'alice@email.com'
+        })
+
+        return response
+            .writeHead(201) //Status Code
+            .end('Create User')
     }
 
     if(method == 'GET' && url == '/users') {
-        return response.end('Read Users')
+        return response
+            .setHeader('Content-type', 'application/json') //Response data type 
+            .end(JSON.stringify(users)) //Must be of type string
     }
 
-    return response.end('Hello World!') //Create HTML Page
+    return response
+        .writeHead(404) //Not Found
+        .end()
 })
 
 server.listen(3333) //Server port
